b97bcfb4f1534272d038e73a417de2fc765de87a
Mohamed Akram <m.akeblersane@esi-sba.dz>
2024-03-15 11:23:00 +0100

    Adding the moderation serviceas well as setting up some configuration for docker and kubernetes

--- comments/index.js
+++ comments/index.js
@@ -10,6 +10,10 @@ app.use(cors())
 
 let commentsByPostId = {}
 
+app.get('/comments/posts', (req, res) => {
+    res.send("hi")
+})
+
 app.get('/posts/:id/comments', (req, res) => {
     res.send(commentsByPostId[req.params.id] || [])
 })
@@ -21,15 +25,17 @@ app.post('/posts/:id/comments', async (req, res) => {
 
     comments.push({
         id: commentId,
-        content
+        content,
+        status: 'pending'
     })
 
-    await axios.post('http://localhost:4005/events', {
-        type: 'commentCreated',
+    await axios.post('http://event-bus-srv:4005/events', {
+        type: 'CommentCreated',
         data: {
             id: commentId,
             content,
-            postId: req.params.id
+            postId: req.params.id,
+            status: 'pending'
         }
     })
 
@@ -38,8 +44,27 @@ app.post('/posts/:id/comments', async (req, res) => {
     res.status(201).send(comments)
 })
 
-app.post('/events', (req, res) => {
-    console.log('Event received of type', req.body.type)
+app.post('/events', async (req, res) => {
+    const {type, data} = req.body
+    if(type==='CommentModerated') {
+        const {id, postId, status, content} = data
+        const comments = commentsByPostId[postId]
+        const comment = comments.find(comment => {
+            return comment.id = id
+        })
+
+        comment.status = status
+
+        await axios.post('http://event-bus-srv:4005/events', {
+            type: 'CommentUpdated',
+            data: {
+                id, 
+                postId,
+                status, 
+                content
+            }
+        })
+    }
     res.send({})
 })
 
--- query/index.js
+++ query/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const axios = require('axios')
 const cors = require ('cors')
 
 const app = express()
@@ -8,12 +9,7 @@ app.use(cors())
 
 let posts = {}
 
-app.get('/posts', (req, res) => {
-    res.send(posts)
-})
-
-app.post('/events', (req, res) => {
-    const {type, data} = req.body;
+const eventHandler = (type, data) => {
     if (type === 'PostCreated') {
         const {id, title} = data
         posts[id] = {
@@ -21,20 +17,51 @@ app.post('/events', (req, res) => {
             title: title,
             comments: []
         }
-    } else {
-        const {id, content, postId} = data
+    } 
+    if(type === 'CommentCreated') {
+        const {id, content, postId, status} = data
         let post = posts[postId]
         post.comments.push({
             id: id,
-            content: content
+            content: content,
+            status: status
         })
     }
+
+    if (type === 'CommentUpdated') {
+        const {id, content, postId, status} = data
+        const post = posts[postId]
+        const comment = post.comments.find(comment => {
+            return comment.id === id
+        })
+
+        comment.content = content
+        comment.status = status
+    }
+}
+
+app.get('/posts', (req, res) => {
+    res.send(posts)
+})
+
+app.post('/events', (req, res) => {
+    const {type, data} = req.body;
+    
+    eventHandler(type, data)
 
     console.log(posts)
 
     res.send({})
 })
 
-app.listen(4002, () => {
+app.listen(4002, async () => {
     console.log('Listening on port 4002')
+
+    const res = await axios.get('http://event-bus-srv:4005/events')
+    const events = res.data
+
+    for (let event of events) {
+        eventHandler(event.type, event.data)
+        
+    }
 })
